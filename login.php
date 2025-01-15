d0b1170b93ab83ef331edb85f39d2032bccbd8ad
Arif fahrudin <arif.fahrudin17@gmail.com>
2025-01-15 21:03:45 +0700

    perubahan pada login.php dan penambahan file proses_login.php

--- login.php
+++ login.php
@@ -7,5 +7,14 @@
 </head>
 <body>
     <h1>Halaman Login Kasir</h1>
+
+    <form action="">
+    <label for="">Username</label>
+    <input type="text"> <br>
+    <label for="">Password</label>
+    <input type="text">
+
+
+    </form>
 </body>
 </html>

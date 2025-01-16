d6377fa8e985c60ca49cdc77c2bd98c14fef3741
Arif fahrudin <arif.fahrudin17@gmail.com>
2025-01-16 21:51:37 +0700

    update aplikasi 16 jan 2025

--- login.php
+++ login.php
@@ -1,20 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+	<title>Login - Aplikasi kasir</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Halaman Login Kasir</h1>
-
-    <form action="">
-    <label for="">Username</label>
-    <input type="text"> <br>
-    <label for="">Password</label>
-    <input type="text">
-
-
-    </form>
+ 
+	
+ 
+	<?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan']=="gagal"){
+			echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+		}
+	}
+	?>
+ 
+	<div class="kotak_login">
+    <h1>Login Aplikasi Kasir<br/>SMKN 1 Kragilan</h1>
+		<p class="tulisan_login">Silahkan login</p>
+ 
+		<form action="cek_login.php" method="post">
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
+ 
+			<label>Password</label>
+			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+ 
+			<input type="submit" class="tombol_login" value="LOGIN">
+ 
+			<br/>
+			<br/>
+			<center>
+				<a class="link" href="https://www.malasngoding.com">kembali</a>
+			</center>
+		</form>
+		
+	</div>
+ 
+ 
 </body>
 </html>
